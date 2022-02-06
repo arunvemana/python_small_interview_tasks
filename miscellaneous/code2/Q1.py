34462012aaaa9fe4a4e6cb80a11e1c0f826a7a05
arunvemana <arunsai4b6@gmail.com>
2022-02-06 15:39:33 +0530

    :hammer:added missed logic

--- miscellaneous/code2/Q1.py
+++ miscellaneous/code2/Q1.py
@@ -1,9 +1,15 @@
-# Todo: Task was in Q1 image.
+# fix: missed the point.
 import re
+import enum
 from itertools import zip_longest
 
 
 def str_to_byte8(cha: str) -> str:
+    """
+    Converting of string into 8-bit binary bits
+    Adding prefix of zero if the binary is not the
+    legth of the 8
+    """
     bin_data = bin(ord(cha)).replace("b", "")
     if len(bin_data) != 8:
         missing = 8 - len(bin_data)
@@ -12,16 +18,53 @@ def str_to_byte8(cha: str) -> str:
     return bin_data
 
 
-def byte_to_str(cha: str) -> str:
-    return str(chr(int(cha, 2)))
+def byte_to_int(cha: str) -> int:
+    """
+    Gives the integer value equivalent to given bytes
+    """
+    return int(cha, 2)
 
 
 def grouper(value: str, byte: int, fill_value: int = '0') -> zip_longest:
+    """
+    Divided the value into given number of equal parts
+    and added fill_value to match the equal_parts
+    """
     args = [iter(value)] * byte
     return zip_longest(*args, fillvalue=fill_value)
 
 
+def mapping():
+    """
+    Generation of mapping data/info
+    """
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                'v',
+                'w', 'x', 'y', 'z']
+    numbers = list(range(0, 10))
+    symbols = ['+', '/']
+    upper_alphabet = [_.upper() for _ in alphabet]
+    mapping = alphabet + upper_alphabet + numbers + symbols
+    _dict = {}
+    for i, x in zip(range(len(mapping)), mapping):
+        _dict[str(i)] = str(x)
+    return _dict
+
+
+def zip_longest_to_str(value: zip_longest) -> list[str]:
+    """
+    take zip_longest type(list of tuples) to list of str
+    """
+    _tem = []
+    for v in value:
+        _tem.append("".join(_ for _ in v))
+    return _tem
+
+
 def let_run(check_string: str):
+    """
+    Main function
+    """
     print(f"Given string is {check_string}")
     data = re.search(r'[^\w/\d+]', check_string)
     if not data:
@@ -29,20 +72,20 @@ def let_run(check_string: str):
         bit8_list = [str_to_byte8(c) for c in check_string]
         bit8_list = "".join(bit8_list)
         value = grouper(bit8_list, 6)
-        _tem = []
-        for v in value:
-            _tem.append("".join(_ for _ in v))
+        _tem = zip_longest_to_str(value)
 
-        return "".join(
-            [byte_to_str(c) for c in _tem]).encode("utf-8",
-                                                   errors='ignore')
+        data = [byte_to_int(c) for c in _tem]
+        data = [MAPPING_INFO[str(v)].value for v in data]
+        return ''.join(data)
     else:
         print(-1)
 
 
 if __name__ == "__main__":
-    # passing case
+    _dict = mapping()
+    MAPPING_INFO = enum.Enum("MAPPING_INFO", _dict)
 
+    # passing case
     Input_string = "ab7+/"
     print(let_run(Input_string))
 
